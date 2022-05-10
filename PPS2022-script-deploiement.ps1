ce85326c8fee233cc07c038b9eef4172fb3935a1
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-10 20:18:33 +0200

    v1

---
+++ PPS2022-script-deploiement.ps1
@@ -0,0 +1,21 @@
+# # #Définir les régles d'éxecution des modules#
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# # #Installer le module Azure Powershell#
+# Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+
+#Demande des identifiants
+$credential = Get-Credential
+
+# Login to the Azure console
+Login-AzAccount -Credential $credential
+
+#Créer un groupe de ressources#
+New-AzResourceGroup -Name ProjetPowershell -Location 'Switzerland North'
+
+#Utiliser une template ARM#
+$TemplateARMdc = ".\PPS2022-Template-ARM-DC.json"
+New-AzResourceGroupDeployment -Name DeploiementProjet -ResourceGroupName ProjetPowershell -TemplateFile $TemplateARMdc
+
+$TemplateARMsrvWEB = ".\PPS2022-Template-ARM-ServeurWeb.json"
+New-AzResourceGroupDeployment -Name DeploiementProjet -ResourceGroupName ProjetPowershell -TemplateFile $TemplateARMsrvWEB
