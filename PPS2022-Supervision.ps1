901b56d9181fad75f499046ffcf4c39379ab8a68
ItsAperoTime <63799557+ItsAperoTime@users.noreply.github.com>
2022-05-17 15:52:06 +0200

    V7
    Added color statut to all the line for more visibility
    Moved option to output to the html from functions X to the main function

--- PPS2022-Supervision.ps1
+++ PPS2022-Supervision.ps1
@@ -26,19 +26,24 @@ $HTMLSupervision = 'CHEMINWINDOWS\HTMLSupervision.html'
 
 
 
-#To do : idéee pourquoi pas faire ligne de la couleur vert ou rouge etc suivant l'usage cpu, ram etc ça serait visuel et cool
-#        La partie warning marche pas en l'état.
 
-function Supervision
+
+function Supervision #Fonction qui contient toutes les autres fonctions pour la supervision
 {
     foreach($Nom in $NomsServeurs)
     {
-        "<tr class=><td class" +$ColorStatut +">"+ $Nom +"</td>" | Out-File $HTMLSupervision -Append utf8
-        AdresseIP
-        OSInformation
         CPUUsage
         RAMUsage
         Statut
+        "<tr class=" +$ColorStatut +"><td class>"+ $Nom +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'affiche le nom du serveur
+        AdresseIP
+        OSInformation
+        CPUUsage
+        "<td class=" +$ColorCPU +">" + $CPUUsage + " %" +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable Usage CPU au HTML avec la couleur en fonction de l'usage CPU
+        RAMUsage
+        "<td class=" +$ColorRAM +">" + $RAMUsage + " %" +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable RAMUsage au HTML avec la couleur qui va bien
+        Statut
+        "<td class=" +$ColorStatut +">" + $Statut +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable Statut au HTML avec la couleur qui va bien
     }
 }
 
@@ -46,6 +51,7 @@ function Supervision
 function AdresseIP #Le but de cette fonction est de connaitre l'IP en connaissant le nom DNS
 {
     $IP = (Resolve-DNSName $Nom -Type A).IPAddress -join " - " #Resolution DNS pour avoir IP, separation de multiresultats par un tiret
+    
     "<td class=" +$ColorStatut +">" + $IP +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute ma variable IP au HTML
 }
 
@@ -61,7 +67,7 @@ function OSInformation
 
 function CPUUsage
 {
-    $CPUUsage = (Get-WmiObject -ComputerName $Nom -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average #Commande pour avoir l'usage du CPU en pourcents
+    $script:CPUUsage = (Get-WmiObject -ComputerName $Nom -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average #Commande pour avoir l'usage du CPU en pourcents
     if ($CPUUsage -le 70) # Si l'usage CPU est en dessous de 70%
     {
         $script:ColorCPU = 'w3-green' #La variable ColorCPU prend la valeur vert
@@ -77,14 +83,13 @@ function CPUUsage
         $script:ColorCPU = 'w3-red'#La variable ColorCPU prend la valeur rouge
     }
 
-    "<td class=" +$ColorCPU +">" + $CPUUsage + " %" +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable Usage CPU au HTML avec la couleur en fonction de l'usage CPU
 }
 
 
 function RAMUsage
 {
     $RAMInfos =  Get-WmiObject -Class WIN32_OperatingSystem -ComputerName $Nom  #Je recupere les informations hardware
-    $RAMUsage = [math]::round((($RAMInfos.TotalVisibleMemorySize - $RAMInfos.FreePhysicalMemory)*100)/ $RAMInfos.TotalVisibleMemorySize) #math pour limiter les decimales; Ensuite calcul entre la ram utilisé et la ram totale pour avoir un pourcentage d'utilisation
+    $script:RAMUsage = [math]::round((($RAMInfos.TotalVisibleMemorySize - $RAMInfos.FreePhysicalMemory)*100)/ $RAMInfos.TotalVisibleMemorySize) #math pour limiter les decimales; Ensuite calcul entre la ram utilisé et la ram totale pour avoir un pourcentage d'utilisation
     
     if ($RAMUsage -le 70) # Si l'usage RAM est en dessous de 70%
     {
@@ -100,8 +105,6 @@ function RAMUsage
     {
         $script:ColorRAM = 'w3-red'#La variable ColorRAM prend la valeur rouge
     }
-    
-    "<td class=" +$ColorRAM +">" + $RAMUsage + " %" +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable RAMUsage au HTML avec la couleur qui va bien
 }
 
 
@@ -109,22 +112,19 @@ function statut
 {
     if(($ColorCPU -eq 'w3-green') -and ($ColorRAM -eq 'w3-green')) #Si la RAM et le CPU sont au vert
     {
-        $Statut = 'Good' #La variable statut vaut "Good"
-        $ColorStatut = 'w3-light-green' #La variable ColorStatut sera verte
+        $script:Statut = 'Good' #La variable statut vaut "Good"
+        $script:ColorStatut = 'w3-pale-green' #La variable ColorStatut sera verte
     }
 
     elseif(($ColorCPU -eq 'w3-red') -and ($ColorRAM -eq 'w3-red')) #Sinon si la RAM et le CPU sont au rouge
     {
-        $Statut = 'KO' #La variable statut vaut "KO"
-        $ColorStatut = 'w3-pale-red' #La variable ColorStatut sera rouge
+        $script:Statut = 'KO' #La variable statut vaut "KO"
+        $script:ColorStatut = 'w3-pale-red' #La variable ColorStatut sera rouge
     }
 
     else #Sinon
     {
-        $Statut = 'Warning' #La variable statut vaut "Warning"
-        $ColorStatut = 'w3-pale-yellow' #La variable ColorStatut sera jaune
+        $script:Statut = 'Warning' #La variable statut vaut "Warning"
+        $script:ColorStatut = 'w3-pale-yellow' #La variable ColorStatut sera jaune
     }
-
-    "<td class=" +$ColorStatut +">" + $Statut +"</td>" | Out-File $HTMLSupervision -Append utf8 #J'ajoute la variable Statut au HTML avec la couleur qui va bien
-
 }
