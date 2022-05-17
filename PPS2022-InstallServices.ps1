416c4c5dde0b9194c4b7f1933d2d1f8014e471db
Jonathan CHAUMONT <jonathan.chaumont28260@gmail.com>
2022-05-17 20:53:54 +0200

    V2

--- PPS2022-InstallServices.ps1
+++ PPS2022-InstallServices.ps1
@@ -15,21 +15,21 @@
 
 
 function PPS2022-InstallAD{
-    
+    $computerCredentials = Get-Credential -Message "Identifants de l'ordinateur"
+    $ADcredentials = Get-Credential -Message "Identifiants pour SafePassword AD"
     $sessionId = New-PSSession -ComputerName 192.168.1.10 -Credential Administrateur
-    Invoke-Command -Session $sessionId -Scriptblock {
-
+    Invoke-Command -Session $sessionId -ArgumentList $ADcredentials.Password -Scriptblock {
     $verifAD = Get-ADDomainController -ErrorAction SilentlyContinue
     
     if(!$verifAD)
     {
-        Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-        Install-ADDSForest -DomainName "PPS2022.local" -DomainNetBiosName "PPS2022" -InstallDns:$true -NoRebootOnCompletion:$true
+        #Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+        Install-ADDSForest -DomainName "PPS2022.local" -DomainNetBiosName "PPS2022" -InstallDns:$true -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $args[0] -Force
     }
 
     }
 
-    Restart-Computer -ComputerName 192.168.1.10 -Wait -For PowerShell -Timeout 300 -Delay 2 -Credential Administrateur -Force
+    Restart-Computer -ComputerName 192.168.1.10 -Wait -For PowerShell -Timeout 300 -Delay 2 -Credential $computerCredentials -Force
     Remove-PSSession $sessionId
     Write-Host -Object "Le serveur a redémarré"
 }
@@ -49,7 +49,28 @@ function PPS2022-InstallDHCP{
 
 }
 
+function PPS2022-InstallIIS{
+
+    $sessionId = New-PSSession -ComputerName 192.168.1.10 -Credential "PPS2022\Administrateur"
+    Invoke-Command -Session $sessionId -Scriptblock {
+    
+    #Installer IIS et les composants
+    Install-WindowsFeature web-server -IncludeManagementTools
+    Install-windowsfeature web-mgmt-service
+    Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
+    install-windowsfeature Web-Asp-Net45  
+    install-windowsfeature Web-ISAPI-Ext
+    install-windowsfeature web-windows-auth
+    Net Stop WMSVC
+    Net Start WMSVC
+    }
+
+
+
+}
+
 PPS2022-InstallAD
-PPS2022-InstallDHCP
+#PPS2022-InstallDHCP
+#PPS2022-InstallIIS
 
 
