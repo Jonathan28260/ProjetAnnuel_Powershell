19a41288ae017d370e77815fc9401fb855cd775f
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-22 22:46:19 +0200

    v4

--- PPS2022-Script-InstallServices.ps1
+++ PPS2022-Script-InstallServices.ps1
@@ -1,14 +1,9 @@
 
 function PPS2022InstallAD{
-    
-    $verifAD = Get-ADDomainController -ErrorAction SilentlyContinue
-    
-    if(!$verifAD)
-    {
-        #Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+        Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
         Install-ADDSForest -DomainName "PPS2022.local" -DomainNetBiosName "PPS2022" -InstallDns:$true -NoRebootOnCompletion:$true -SafeModeAdministratorPassword  -Force
-    }
-    Restart-Computer
+        Restart-Computer
 }
 PPS2022InstallAD
 
