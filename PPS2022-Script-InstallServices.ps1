d1c56e9f5f3c91433e9aac98edbca9fd0d09a0c1
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-22 22:28:18 +0200

    v3

--- PPS2022-Script-InstallServices.ps1
+++ PPS2022-Script-InstallServices.ps1
@@ -12,4 +12,12 @@ function PPS2022InstallAD{
 }
 PPS2022InstallAD
 
-
+function PPS2022InstallDHCP{
+      
+    Install-WindowsFeature DHCP -IncludeManagementTools
+    Add-DHCPServerInDC -DNSName SRV-AD.PPS2022.local
+    Set-DHCPServerv4OptionValue -DNSServer 192.168.1.10 -DNSDomain PPS2022.local -Router 192.168.1.1
+    Add-DHCPServerv4Scope -Name "Pool PPS2022" -StartRange 192.168.1.50 -EndRange 192.168.1.100 -SubnetMask 255.255.255.0 -Description "Plage DHCP pour projet PPS2022"
+    
+}
+PPS2022InstallDHCP
