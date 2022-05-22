3f7dfb4fda59a017914a276566790b9efde0cfc2
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-22 23:58:57 +0200

    v7

--- PPS2022-Script-InstallServices.ps1
+++ PPS2022-Script-InstallServices.ps1
@@ -1,15 +1,14 @@
 param
 (
-      [string]$ADPassword
-
+$ADPassword
 )
 
 function PPS2022InstallAD{
 
-        $SafeModeAdministratorPassword= ConvertTo-SecureString $Password  -AsPlainText  -Force
         Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
         Install-ADDSForest -DomainName "PPS2022.local" -DomainNetBiosName "PPS2022" -InstallDns:$true -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $SafeModeAdministratorPassword -Force
         Restart-Computer
+        
 }
 PPS2022InstallAD
 
