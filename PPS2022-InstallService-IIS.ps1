12eee37ba82af3bb5968a834d06270eee3da432a
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-23 02:05:55 +0200

    v14

--- PPS2022-InstallService-IIS.ps1
+++ PPS2022-InstallService-IIS.ps1
@@ -10,5 +10,19 @@ function PPS2022installIIS{
     Net Stop WMSVC
     Net Start WMSVC
     New-NetFirewallRule -Name "IIS80" -DisplayName "IIS80" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 80 -Protocol TCP
+    
     }
 PPS2022installIIS
+
+function ScheduledTask{
+
+    New-item -Path "C:\" -Name "Scripts" -ItemType "directory"
+    ADD-content -path "C:\Scripts\PPS2022-Loop-Supervision.ps1" -value "PPS2022-Loop"
+    cd C:\Scripts
+    Invoke-WebRequest -Uri "https://github.com/Jonathan28260/ProjetAnnuel_Powershell/blob/main/PPS2022-Supervision.ps1" 
+
+    $action=New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass C:\Scripts\PPS2022-Loop-Supervision.ps1"
+    $trigger=New-ScheduledTaskTrigger -Once -AtStartup
+    Register-ScheduledTask -TaskName "Supervision" -Trigger $trigger -Action $action -Description "Supervision" -User "PPS2022\Administrateur"
+
+    }
