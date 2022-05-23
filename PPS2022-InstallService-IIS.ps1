33fff63e062fe9f9f46a9e3377b3b6493c15bc7e
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-23 22:34:36 +0200

    v32

--- PPS2022-InstallService-IIS.ps1
+++ PPS2022-InstallService-IIS.ps1
@@ -23,8 +23,7 @@ function ScheduledTask{
     New-item -Path "C:\" -Name "Scripts" -ItemType "directory"
     ADD-content -path "C:\Scripts\PPS2022-Loop-Supervision.ps1" -value 'while($true){ C:\Scripts\PPS2022-Supervision.ps1 
     sleep -seconds 10 }'
-    ADD-content -path "C:\Scripts\NomsServeurs.txt" -value "PPS2022-SRV-DC
-    PPS2022-SRV-IIS"
+    ADD-content -path "C:\Scripts\NomsServeurs.txt" -value "PPS2022-SRV-DC `nPPS2022-SRV-IIS"
     cd C:\Scripts
     Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/Jonathan28260/ProjetAnnuel_Powershell/main/PPS2022-Supervision.ps1" -OutFile C:\Scripts\PPS2022-Supervision.ps1 
 
