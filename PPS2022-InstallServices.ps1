110293d25f52426dde2f9c98b491be17dced08f3
Jonathan28260 <63723882+Jonathan28260@users.noreply.github.com>
2022-05-28 16:27:59 +0200

    V5

--- PPS2022-InstallServices.ps1
+++ PPS2022-InstallServices.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = "stop"
+$ErrorActionPreference = "stop"
 
 function PPS2022TestWinRM{
 
@@ -28,30 +28,27 @@ function PPS2022InstallAD{
 
     #Initialisation de la session Powershell à distance
     Try{
-        #$sessionId = New-PSSession -ComputerName $computerName -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck) -Credential $computerCredentials
-        $sessionId = New-PSSession -ComputerName pps2022srvdc.switzerlandnorth.cloudapp.azure.com -Credential Administrateur
+        $sessionId = New-PSSession -ComputerName pps2022srvdc.switzerlandnorth.cloudapp.azure.com -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck) -Credential "PPS2022\Administrateur"
     }
 
     Catch{
         Write-Host $Error[0].Exception.Message -ForegroundColor Red
     }
-
-    #Recuperation du SafePassword pour la foret ActiveDirectory
-    $safePassword = Read-Host "Entrer le SafePassword pour la foret Active Directory" -AsSecureString
     
     #Si la PSSession a bien été initialisée alors on envoi les commandes suivantes sur le serveur distant
     if($sessionId){
+
+    #Recuperation du SafePassword pour la foret ActiveDirectory
+    $safePassword = Read-Host "Entrer le SafePassword pour la foret Active Directory" -AsSecureString
+
     Invoke-Command -Session $sessionId -ArgumentList $safePassword -Scriptblock {
 
         $verifAD = Get-ADDomainController -ErrorAction SilentlyContinue
     
         if(!$verifAD)
         {
-            $domainName = Read-Host -Prompt "Veuillez entrer le nom de domaine complet (Ex: Test.local)"
-            $netbiosName = Read-Host -Prompt "Veuillez entrer le nom Netbios (Ex: Test)"
             Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-            Install-ADDSForest -DomainName $domainName -DomainNetBiosName $netbiosName -InstallDns:$true -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $args[0] -Force
-            Restart-Computer
+            Install-ADDSForest -DomainName PPS2022.local -DomainNetBiosName PPS2022 -InstallDns:$true -NoRebootOnCompletion:$false -SafeModeAdministratorPassword $args[0] -Force
         }
 
     }
@@ -68,26 +65,33 @@ function PPS2022InstallAD{
 }
 
 function PPS2022InstallDHCP{
-    $computerCredentials = Get-Credential -Message "Identifants de l'ordinateur distant"
-    $computerName = Read-Host -Prompt "Veuillez entrer le nom du serveur auquel vous souhaitez accéder"
 
     try {
-        $sessionId = New-PSSession -ComputerName $computerName -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck) -Credential $computerCredentials
+        $sessionId = New-PSSession -ComputerName pps2022srvdc.switzerlandnorth.cloudapp.azure.com -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck) -Credential "PPS2022\Administrateur"
 
     }
     catch {
         Write-Host $Error[0].Exception.Message -ForegroundColor Red
     }
 
-    Invoke-Command -Session $sessionId -Scriptblock {
-    Install-WindowsFeature DHCP -IncludeManagementTools
-    Add-DHCPServerInDC -DNSName PPS2022-SRV-DC.PPS2022.local
-    Set-DHCPServerv4OptionValue -DNSServer 192.168.1.4 -DNSDomain PPS2022.local -Router 192.168.1.1
-    Add-DHCPServerv4Scope -Name "Pool PPS2022" -StartRange 192.168.1.50 -EndRange 192.168.1.100 -SubnetMask 255.255.255.0 -Description "Plage DHCP pour projet PPS2022"
-    }
+    if($sessionId){
+        Try{
+            Invoke-Command -Session $sessionId -Scriptblock {
+                Install-WindowsFeature DHCP -IncludeManagementTools
+                Add-DHCPServerInDC -DNSName ([System.Net.Dns]::GetHostByName($env:computerName).HostName)
+                Set-DHCPServerv4OptionValue -DNSServer 192.168.1.10 -DNSDomain PPS2022.local -Router 192.168.1.1
+                Add-DHCPServerv4Scope -Name "Pool PPS2022" -StartRange 192.168.1.50 -EndRange 192.168.1.100 -SubnetMask 255.255.255.0 -Description "Plage DHCP pour projet PPS2022"
+            } -ErrorAction SilentlyContinue
 
-    Remove-PSSession $sessionId
-    Write-Host -Object "Le service DHCP a ete installe"
+        Write-Host -Object "Le service DHCP a ete installe" -ForegroundColor Green
+        }
+
+        catch{
+            Write-Host $Error[0].Exception.Message -ForegroundColor Red
+        }
+
+        Remove-PSSession $sessionId
+    }
 }
 
 function DisplayMenu{
@@ -97,7 +101,7 @@ function DisplayMenu{
         write-host "1. Verifier WinRM sur le poste local"
         write-host "2. Installer le service Active Directory"
         write-host "3. Installer le service DHCP"
-        write-host "x. Exit"
+        write-host "4. Exit"
         write-host "--------------------------------------------------------"
         $choix = read-host "Faire un choix"
         switch ($choix){
