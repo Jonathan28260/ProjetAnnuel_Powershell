22579787defd312b26cf66070630233a8a1caa69
Jonathan28260 <63723882+Jonathan28260@users.noreply.github.com>
2022-05-28 14:40:44 +0200

    V2

--- Creation UO_Utilisateur.ps1
+++ Creation UO_Utilisateur.ps1
@@ -1,14 +1,19 @@
 # Configuration / Paramétrage AD
 
 function PPS2022CreationUO {
-    New-ADOrganizationalUnit "Direction" -Path "OU=DC=PPS2022,DC=local"
-    New-ADGroup -Name "Direction Users" -GroupeScope Global -Path "OU=Direction,DC=PPS2022,DC=local"
 
-    New-ADOrganizationalUnit "Marketing" -Path "OU=DC=PPS2022,DC=local"
-    New-ADGroup -Name "Marketing Users" -GroupeScope Global -Path "OU=Marketing,DC=PPS2022,DC=local"
+    $sessionId = New-PSSession -ComputerName pps2022testdc.switzerlandnorth.cloudapp.azure.com -Credential PPS2022\Administrateur
+    Invoke-Command -Session $sessionId -Scriptblock {
 
-    New-ADOrganizationalUnit "Compta" -Path "OU=DC=PPS2022,DC=local"
-    New-ADGroup -Name "Compta Users" -GroupeScope Global -Path "OU=Compta,DC=PPS2022,DC=local"
+    New-ADOrganizationalUnit "Direction" -Path "DC=PPS2022,DC=local"
+    New-ADGroup -Name "Direction Users" -GroupScope Global -Path "OU=Direction,DC=PPS2022,DC=local"
+
+    New-ADOrganizationalUnit "Marketing" -Path "DC=PPS2022,DC=local"
+    New-ADGroup -Name "Marketing Users" -GroupScope Global -Path "OU=Marketing,DC=PPS2022,DC=local"
+
+    New-ADOrganizationalUnit "Comptabilite" -Path "DC=PPS2022,DC=local"
+    New-ADGroup -Name "Compta Users" -GroupScope Global -Path "OU=Comptabilite,DC=PPS2022,DC=local"
+    }
 }
 
 # Paramétrage comptes utilisateurs
@@ -21,40 +26,50 @@ function PPS2022CreationUser {
 
 # Ci-joint le chemin d'accès vers le fichier d'importation CSV
 
-$UtilisateurAD = Import-csv C:\Users\scripts\NouveauxUtilisateurs.CSV
+$UtilisateurAD = Import-csv .\NouveauxUtilisateurs.CSV -Delimiter ";"
 
-function PPS2022CreationUtilisateurs
+function PPS2022CreationUtilisateurs{
 
-foreach ($Utilisateur in $UtilisateurAD)
-{
+    $sessionId = New-PSSession -ComputerName pps2022testdc.switzerlandnorth.cloudapp.azure.com -Credential PPS2022\Administrateur
+    $commande = Invoke-Command -Session $sessionId -ArgumentList $UtilisateurAD -Scriptblock {
 
-       $Username    = $Utilisateur.identifiant
-       $Password    = $Utilisateur.motdepasse
-       $Firstname   = $Utilisateur.prenom
-       $Lastname    = $Utilisateur.nom
-       $OU           = $Utilisateur.ou
+        foreach ($Utilisateur in $args[0])
+        {
 
-       # Nous allons vérifier si l'utilisateur existe déjà et renvoyer un message dans le cas ou il existe
-       # S'il n'existe pas il sera créé
-       # Le compte sera créé dans I’unité d’organisation indiquée dans la variable $OU du fichier CSV 
+            $Username    = $Utilisateur.identifiant
+            $Password    = $Utilisateur.motdepasse
+            $Firstname   = $Utilisateur.prenom
+            $Lastname    = $Utilisateur.nom
+            $OU          = $Utilisateur.ou
 
-       if (Get-ADUser -F {SamAccountName -eq $Username})
-       {
-               Write-Warning "L'utilisateur $Username existe déjà dans l'Active Directory."
+            # Nous allons vérifier si l'utilisateur existe déjà et renvoyer un message dans le cas ou il existe
+            # S'il n'existe pas il sera créé
+            # Le compte sera créé dans I’unité d’organisation indiquée dans la variable $OU du fichier CSV 
+
+            if (Get-ADUser -F {SamAccountName -eq $Username})
+            {
+                Write-Warning "L'utilisateur $Username existe déjà dans l'Active Directory."
+            }
+            else
+            {
+                New-ADUser `
+                -SamAccountName $Username `
+                -UserPrincipalName "$Username@PPS2022.local" `
+                -Name "$Firstname $Lastname" `
+                -GivenName $Firstname `
+                -Surname $Lastname `
+                -Enabled $True `
+                -ChangePasswordAtLogon $True `
+                -DisplayName "$Lastname, $Firstname" `
+                -Path "OU=$OU,DC=PPS2022,DC=local" `
+                -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
        }
-       else
-       {
-              New-ADUser `
-            -SamAccountName $Username `
-            -UserPrincipalName "$Username@PPS2022.local" `
-            -Name "$Firstname $Lastname" `
-            -GivenName $Firstname `
-            -Surname $Lastname `
-            -Enabled $True `
-            -ChangePasswordAtLogon $True `
-            -DisplayName "$Lastname, $Firstname" `
-            -Path $OU `
-            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+    }
+    }
 
-       }
+    return $commande
 }
+
+# PPS2022CreationUO
+PPS2022CreationUtilisateurs
