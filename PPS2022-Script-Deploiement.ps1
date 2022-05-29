19c43f5af436486ff08d0072fb6da04702a9307a
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-29 03:46:22 +0200

    v1

--- PPS2022-Script-Deploiement.ps1
+++ PPS2022-Script-Deploiement.ps1
@@ -1,13 +1,13 @@
-function VerificationModuleAz {
-    # # #Définir les régles d'éxecution des modules#
-    # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+function PPS2022VerificationModuleAz {
+    #Définir les régles d'éxecution des modules#
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-    # # #Installer le module Azure Powershell#
+    #Installer le module Azure Powershell#
     Install-Module -Name Az -AllowClobber -Scope AllUsers -Force 
 }
 
 
-function CreationRG {
+function PPS2022CreationRG {
     #Demande des identifiants
     $credential = Get-Credential -Message "Veuillez entrer les identifiants de votre compte Azure"
 
@@ -17,16 +17,43 @@ function CreationRG {
     #Créer un groupe de ressources#
     New-AzResourceGroup -Name "PPS2022-RG" -Location 'Switzerland North'
 }
-CreationRG
 
 
-function DeploiementARM {
+
+function PPS2022DeploiementARM {
     #Utilisation des templates ARM#
     $TemplateARM = ".\PPS2022-Template-ARM-DC+IIS.json"
     New-AzResourceGroupDeployment -Name "PPS2022-ARM-DC" -ResourceGroupName "PPS2022-RG" -TemplateFile $TemplateARM
 }
-DeploiementARM
 
 
-Remove-AzResourceGroup -Name "PPS2022-RG"
-Remove-AzResourceGroup -Name "NetworkWatcherRG"
+function PPS2022SuppressionRessources {
+    #Suppression du groupe de ressources et donc de chaque ressources crée#
+    Remove-AzResourceGroup -Name "PPS2022-RG"
+    Remove-AzResourceGroup -Name "NetworkWatcherRG"
+}
+
+
+function PPS2022ChoixMenu {
+    $continue = $true
+    while ($continue){
+        write-host "---------------SCRIPT de Déploiement--------------------"
+        write-host "1. Verifier le module Az sur le poste local"
+        write-host "2. Connexion à Azure et création RG"
+        write-host "3. Déploiement ARM"
+        write-host "4. Supression des ressources"
+        write-host "5. Exit"
+        write-host "--------------------------------------------------------"
+        $choix = read-host "Faire un choix"
+        switch ($choix){
+            1{PPS2022VerificationModuleAz}
+            2{PPS2022CreationRG}
+            3{PPS2022DeploiementARM}
+            4{PPS2022SuppressionRessources}
+            5{$continue = $false}
+
+        default {Write-Host "Choix invalide"-ForegroundColor Red}
+        }
+    }
+}
+PPS2022ChoixMenu
