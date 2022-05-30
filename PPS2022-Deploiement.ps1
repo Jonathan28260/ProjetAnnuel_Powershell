78f271c8ba2f14735cb45f9703e4b796648d89d1
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-05-30 09:23:26 +0200

    v4

--- PPS2022-Deploiement.ps1
+++ PPS2022-Deploiement.ps1
@@ -1,13 +1,13 @@
-function VerificationModuleAz {
+function PPS2022VerificationModuleAz {
     # # #Définir les régles d'éxecution des modules#
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
     # # #Installer le module Azure Powershell#
-    Install-Module -Name Az -AllowClobber -Scope AllUsers -Force 
+    Install-Module -Name Az -Scope AllUsers -Force 
 }
 
 
-function CreationRG {
+function PPS2022CreationRG {
     #Demande des identifiants
     $credential = Get-Credential -Message "Veuillez entrer les identifiants de votre compte Azure"
 
@@ -20,14 +20,14 @@ function CreationRG {
 # CreationRG
 
 
-function DeploiementARM {
+function PPS2022DeploiementARM {
     #Utilisation des templates ARM#
     $TemplateARM = ".\PPS2022-Template-ARM-DC+IIS.json"
     New-AzResourceGroupDeployment -Name "PPS2022-ARM" -ResourceGroupName "PPS2022-RG" -TemplateFile $TemplateARM -Verbose
 }
-# DeploiementARM
 
-function SuppressionRessources{
+
+function PPS2022SuppressionRessources{
     #Suppresion des ressources Azure
     Remove-AzResourceGroup -Name "PPS2022-RG"
     Remove-AzResourceGroup -Name "NetworkWatcherRG"   
@@ -45,10 +45,10 @@ function DisplayMenu{
         write-host "--------------------------------------------------------"
         $choix = read-host "Faire un choix"
         switch ($choix){
-            1{VerificationModuleAz}
-            2{CreationRG}
-            3{DeploiementARM}
-            4{SuppressionRessources}
+            1{PPS2022VerificationModuleAz}
+            2{PPS2022CreationRG}
+            3{PPS2022DeploiementARM}
+            4{PPS2022SuppressionRessources}
             5{$continue = $false}
 
         default {Write-Host "Choix invalide"-ForegroundColor Red}
